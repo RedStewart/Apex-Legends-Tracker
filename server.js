1d8d3a6629dbb72f1f1620716ccf129ee556398e
Reed Stewart <reedostewart@gmail.com>
2019-08-08 14:47:04 +1200

    Backend route setup using third party apex api. Routes created to hit from front end

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-const dotenv = require('dotenv');
-
-// Load env
-dotenv.config({ path: './config.env' });
+const dotenv = require('dotenv')
+dotenv.config({path: './config.env'})
 
 const app = express();
 
-app.listen(5000, () => {
-  console.log('App listening on port 5000...');
+// Dev logging
+if(process.env.NODE_ENV === 'development'){
+  app.use(morgan('dev'))
+}
+
+// Profile route
+app.use('/api/v1/profile', require('./routes/profile'));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}...`);
 });
