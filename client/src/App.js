783ef0ef2933c05a2e6f4928b61d0fa31e0e64f4
Reed Stewart <reedostewart@gmail.com>
2019-08-08 16:02:06 +1200

    Initialised Ant Design as a UI framework

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { DatePicker } from 'antd';
 import './App.css';
 
 function App() {
   return (
     <div className='App'>
       <h1>Apex Legends Tracker</h1>
+      <DatePicker />
     </div>
   );
 }
