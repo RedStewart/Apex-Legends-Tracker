3cceff91afd4973892aaf2216e01fe98be0e96af
Reed Stewart <reedostewart@gmail.com>
2019-08-14 23:05:53 +1200

    Cleaned up App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,54 +1,28 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import AlertState from './context/alert/AlertState';
 import ProfileState from './context/profile/ProfileState';
-import Search from './components/Search';
 import Profile from './components/Profile';
 import SideBar from './components/layout/SideBar';
-import BackgroundImage from './components/layout/BackgroundImage';
 import './App.css';
 
 const App = () => {
-  // useEffect(() => {
-  //   document.getElementById('page-bg').style.background = `url(${image})`;
-  // }, []);
-
   return (
     <ProfileState>
       <AlertState>
         <Router>
-          {/* <div id='page-bg' /> */}
-          {/* <BackgroundImage backgroundType={'landing'} /> */}
-          <div>
-            <Switch>
-              <Route exact path='/' component={SideBar} />
-              <Route
-                exact
-                path='/profile/:platform/:gamertag'
-                component={Profile}
-              />
-            </Switch>
-          </div>
+          <Switch>
+            <Route exact path='/' component={SideBar} />
+            <Route
+              exact
+              path='/profile/:platform/:gamertag'
+              component={Profile}
+            />
+          </Switch>
         </Router>
       </AlertState>
     </ProfileState>
   );
 };
-
-const imageArr = [
-  'bangalore',
-  'bloodhound',
-  'caustic',
-  'gibraltar',
-  'lifeline',
-  'mirage',
-  'octane',
-  'pathfinder',
-  'wraith'
-];
-
-const image = require(`./images/${
-  imageArr[Math.floor(Math.random() * imageArr.length)]
-}.png`);
 
 export default App;
