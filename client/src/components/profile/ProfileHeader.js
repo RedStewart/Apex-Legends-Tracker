3e39187f8dbfc60153a91190cc152a97ade8b733
Reed Stewart <reedostewart@gmail.com>
2019-08-15 23:42:33 +1200

    Added some custom images for the profile header section

--- client/src/components/profile/ProfileHeader.js
+++ client/src/components/profile/ProfileHeader.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import avatar from '../../images/avatar.png';
+import levelIcon from '../../images/level/100.png';
 import rankedIcon from '../../images/ranked/diamond.png';
 
 const ProfileHeader = () => {
   return (
     <div className='profile-header'>
-      <div className='grid-3'>
+      <div className='grid-4'>
         <div className='header-item'>
           <img className='round-img' src={avatar} alt='platform avatar' />
-          <div>
+          <div className='header-name'>
             <h1>r33333d0</h1>
             <i className='fab fa-windows icon' />
           </div>
+        </div>
+
+        <div className='header-item'>
+          <img className='round-img' src={levelIcon} alt='level icon' />
+          <h3>Level</h3>
+          <p>1562</p>
         </div>
 
         <div className='header-item'>
@@ -24,11 +31,11 @@ const ProfileHeader = () => {
 
         <div className='header-item stats-header-outer'>
           <div className='stats-header-inner'>
-            <p>Level</p>
-            <h2>69</h2>
-            <br />
             <p>Overall Kills</p>
             <h2>1337</h2>
+            <br />
+            <p>KDR</p>
+            <h2>0.85</h2>
           </div>
         </div>
       </div>
