c0b855635cd3dfd945aee4367ab6757cab89b44b
Reed Stewart <reedostewart@gmail.com>
2019-08-16 19:21:13 +1200

    Legend ui showing the legends that the user has played and styling nearly complete

--- client/src/components/profile/LegendItem.js
+++ client/src/components/profile/LegendItem.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { Card } from 'antd';
 
 const LegendItem = ({ legend }) => {
+  const { Meta } = Card;
+
   return (
     <div className='legend-item'>
       <div className='legend-name-container'>
@@ -11,8 +14,47 @@ const LegendItem = ({ legend }) => {
           alt='legend'
         />
       </div>
+      {/* <Card className='legend-content'>
+        <Card.Grid style={gridStyle}>
+          <h2>Season 1 Wins</h2>
+          <p>1337</p>
+        </Card.Grid>
+        <Card.Grid style={gridStyle}>
+          <h2>Kills</h2>
+          <p>1337</p>
+        </Card.Grid>
+        <Card.Grid style={gridStyle}>
+          <h2>Kills</h2>
+          <p>1337</p>
+        </Card.Grid>
+        <Card.Grid style={gridStyle}>
+          <h2>Kills</h2>
+          <p>1337</p>
+        </Card.Grid>
+      </Card> */}
+      <div className='legend-content'>
+        <div className='grid-2'>
+          <div>
+            <h2>Season 1 Wins</h2>
+            <p>1337</p>
+          </div>
+          <div>
+            <h2>Kills</h2>
+            <p>1337</p>
+          </div>
+          <div>
+            <h2>Kills</h2>
+            <p>1337</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
+};
+
+const gridStyle = {
+  width: '50%',
+  textAlign: 'center'
 };
 
 export default LegendItem;
