1706add58957e1bb37d12b65e45ab04d0a420392
Reed Stewart <reedostewart@gmail.com>
2019-08-18 20:58:11 +1200

    Prepare for deploy

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
 import ProfileState from './context/profile/ProfileState';
 import Profile from './components/profile/Profile';
 import SideBar from './components/layout/SideBar';
@@ -16,6 +21,7 @@ const App = () => {
             path='/profile/:platform/:gamertag'
             component={Profile}
           />
+          <Route render={() => <Redirect to='/' />} />
         </Switch>
       </Router>
     </ProfileState>
--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const path = require('path');
 const morgan = require('morgan');
-const dotenv = require('dotenv')
-dotenv.config({path: './config.env'})
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
 
 const app = express();
 
 // Dev logging
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
 }
 
 // Profile route
 app.use('/api/v1/profile', require('./routes/profile'));
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 const port = process.env.PORT || 5000;
 
